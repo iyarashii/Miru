a048126e6b3b87d0cd7c2a332940435de5c6c418
Iyarashii <strider1337v@gmail.com>
2021-11-18 02:36:45 +0100

    Add LoadLastSyncedData test case for empty DbSet

--- Miru.Tests/DatabaseTests/MiruDbServiceTests.cs
+++ Miru.Tests/DatabaseTests/MiruDbServiceTests.cs
@@ -19,6 +19,30 @@ namespace Miru.Tests.DatabaseTests
 {
     public class MiruDbServiceTests
     {
+        private MiruDbService SetupMiruDbServiceMock(AutoMock mock, IQueryable<SyncedMyAnimeListUser> data)
+        {
+            // https://docs.microsoft.com/en-us/ef/ef6/fundamentals/testing/mocking
+            // wire up the IQueryable implementation for the DbSet more info in the link above
+            var mockUserSet = new Mock<DbSet<SyncedMyAnimeListUser>>();
+            mockUserSet.As<IQueryable<SyncedMyAnimeListUser>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockUserSet.As<IQueryable<SyncedMyAnimeListUser>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockUserSet.As<IQueryable<SyncedMyAnimeListUser>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockUserSet.As<IQueryable<SyncedMyAnimeListUser>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<MiruDbContext>();
+            mockContext.Setup(m => m.SyncedMyAnimeListUsers).Returns(mockUserSet.Object);
+            var mockEventHandler = new Mock<EventHandler<DateTime>>();
+            var mockUsernameEventHandler = new Mock<EventHandler<string>>();
+
+            Func<IMiruDbContext> mockFunc = () => { return mockContext.Object; };
+
+            var cls = mock.Create<MiruDbService>(new NamedParameter("createMiruDbContext", mockFunc));
+            cls.UpdateSyncDate += mockEventHandler.Object;
+            cls.UpdateCurrentUsername += mockUsernameEventHandler.Object;
+
+            return cls;
+        }
+
         [Fact]
         public void LoadLastSyncedData_SetsCorrectValuesFromDb()
         {
@@ -32,24 +56,8 @@ namespace Miru.Tests.DatabaseTests
                     new SyncedMyAnimeListUser { Username = testUsername, SyncTime = testSyncDate },
                 }.AsQueryable();
 
-                // https://docs.microsoft.com/en-us/ef/ef6/fundamentals/testing/mocking
-                // wire up the IQueryable implementation for the DbSet more info in the link above
-                var mockUserSet = new Mock<DbSet<SyncedMyAnimeListUser>>();
-                mockUserSet.As<IQueryable<SyncedMyAnimeListUser>>().Setup(m => m.Provider).Returns(data.Provider);
-                mockUserSet.As<IQueryable<SyncedMyAnimeListUser>>().Setup(m => m.Expression).Returns(data.Expression);
-                mockUserSet.As<IQueryable<SyncedMyAnimeListUser>>().Setup(m => m.ElementType).Returns(data.ElementType);
-                mockUserSet.As<IQueryable<SyncedMyAnimeListUser>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+                var cls = SetupMiruDbServiceMock(mock, data);
 
-                var mockContext = new Mock<MiruDbContext>();
-                mockContext.Setup(m => m.SyncedMyAnimeListUsers).Returns(mockUserSet.Object);
-                var mockEventHandler = new Mock<EventHandler<DateTime>>();
-                var mockUsernameEventHandler = new Mock<EventHandler<string>>();
-
-                Func<IMiruDbContext> mockFunc = () => { return mockContext.Object; };
-
-                var cls = mock.Create<MiruDbService>(new NamedParameter("createMiruDbContext", mockFunc));
-                cls.UpdateSyncDate += mockEventHandler.Object;
-                cls.UpdateCurrentUsername += mockUsernameEventHandler.Object;
                 //cls.UpdateSyncDate += new EventHandler<DateTime>((x, y) => { });
 
                 // Act
@@ -58,6 +66,26 @@ namespace Miru.Tests.DatabaseTests
                 // Assert
                 Assert.Equal(testSyncDate, cls.SyncDateData);
                 Assert.Equal(testUsername, cls.CurrentUsername);
+            }
+        }
+
+        [Fact]
+        public void LoadLastSyncedData_WhenSyncedMyAnimeListUsersIsEmpty_DoesNotSetProperties()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // Arrange
+                DateTime testSyncDate = default;
+                var data = new List<SyncedMyAnimeListUser>().AsQueryable();
+
+                var cls = SetupMiruDbServiceMock(mock, data);
+
+                // Act
+                cls.LoadLastSyncedData();
+
+                // Assert
+                Assert.Equal(testSyncDate, cls.SyncDateData);
+                Assert.Null(cls.CurrentUsername);
             }
         }
 
