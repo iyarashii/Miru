a0339b249381c7a726116d0b73b738e62d430ddd
Iyarashii <strider1337v@gmail.com>
2022-01-29 00:56:26 +0100

    Update ChangeDisplayedAnimeList test

--- Miru.Tests/DatabaseTests/MiruDbServiceTests.cs
+++ Miru.Tests/DatabaseTests/MiruDbServiceTests.cs
@@ -20,7 +20,6 @@ namespace Miru.Tests.DatabaseTests
 {
     public class MiruDbServiceTests
     {
-        public bool EventExecuted { get; set; } = false;
         private IMiruDbService SetupMiruDbServiceMock(Mock<IMiruDbContext> mockContext, AutoMock mock,[Optional] IQueryable<SyncedMyAnimeListUser> userDbSetData, [Optional] IQueryable<MiruAnimeModel> miruAnimeModelDbSetData)
         {
             mockContext.Setup(s => s.ExecuteSqlCommand("TRUNCATE TABLE [MiruAnimeModels]")).Returns(0);
@@ -55,11 +54,9 @@ namespace Miru.Tests.DatabaseTests
 
             var cls = mock.Create<MiruDbService>(new NamedParameter("createMiruDbContext", mockFunc));
 
-            //var mockSortedAnimeListEventHandler = new Mock<MiruDbService.SortedAnimeListEventHandler>();
 
             cls.UpdateSyncDate += mockEventHandler.Object;
             cls.UpdateCurrentUsername += mockUsernameEventHandler.Object;
-            cls.UpdateAnimeListEntriesUI += (x, y) => EventExecuted = true;
 
             return cls;
         }
@@ -170,19 +167,20 @@ namespace Miru.Tests.DatabaseTests
             using (var mock = AutoMock.GetLoose())
             {
                 // Arrange
+                var eventExecuted = false;
                 var mockContext = new Mock<IMiruDbContext>();
                 var data = new List<MiruAnimeModel>
                 {
                     new MiruAnimeModel {Title = "nnn" },
                 }.AsQueryable();
                 var cls = SetupMiruDbServiceMock(mockContext, mock, miruAnimeModelDbSetData: data);
+                cls.UpdateAnimeListEntriesUI += (x, y) => eventExecuted = true;
 
                 // Act
                 cls.ChangeDisplayedAnimeList(It.IsAny<AnimeListType>(), It.IsAny<TimeZoneInfo>(), It.IsAny<MiruLibrary.AnimeType>(), It.IsAny<string>());
-                //Mock.Of<TimeZoneInfo>() -- System.NotSupportedException : Type to mock must be an interface, a delegate, or a non-sealed, non-static class.
 
                 // Assert
-                Assert.True(EventExecuted);
+                Assert.True(eventExecuted);
             }
         }
     }
