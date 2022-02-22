d9b184fd462b30d3075541d6025501796a1d24c8
Iyarashii <strider1337v@gmail.com>
2022-02-22 17:36:35 +0100

    Add CurrentSeasonModel tests class

---
+++ Miru.Tests/ModelsTests/CurrentSeasonModelTests.cs
@@ -0,0 +1,34 @@
+﻿using Autofac.Extras.Moq;
+using JikanDotNet;
+using MiruLibrary.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Miru.Tests.ModelsTests
+{
+    public class CurrentSeasonModelTests
+    {
+        [Fact]
+        public async void GetCurrentSeasonList_OnHttpRequestException_ReturnsFalse()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // Arrange
+                mock.Mock<IJikan>().Setup(x => x.GetSeason()).ThrowsAsync(new HttpRequestException());
+                var sut = mock.Create<CurrentSeasonModel>();
+
+                // Act
+                var result = await sut.GetCurrentSeasonList(It.IsAny<int>());
+
+                // Assert
+                Assert.False(result);
+            }
+        }
+    }
+}
