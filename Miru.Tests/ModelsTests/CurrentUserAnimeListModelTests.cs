a1c2e8da816b5289591d49a4719985d282bf8875
Iyarashii <strider1337v@gmail.com>
2022-02-28 17:43:52 +0100

    Add tests for CurrentUserAnimeListModel class

---
+++ Miru.Tests/ModelsTests/CurrentUserAnimeListModelTests.cs
@@ -0,0 +1,42 @@
+﻿using Autofac;
+using Autofac.Extras.Moq;
+using JikanDotNet;
+using JikanDotNet.Exceptions;
+using MiruLibrary.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Miru.Tests.ModelsTests
+{
+    public class CurrentUserAnimeListModelTests
+    {
+        [Fact]
+        public async void GetCurrentUserAnimeList_OnHttpRequestException_ReturnsFalseAndExpectedErrorMessage()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // Arrange
+                mock.Mock<IJikan>()
+                    .Setup(x => x.GetUserAnimeList(It.IsAny<string>(), UserAnimeListExtension.Watching))
+                    .ThrowsAsync(new HttpRequestException());
+                var expectedErrorMessage = "Problems with internet connection!";
+                var sut = mock.Create<CurrentUserAnimeListModel>();
+
+                // Act
+                var (result, errorMessage) = await sut.GetCurrentUserAnimeList(It.IsAny<string>());
+
+                // Assert
+                Assert.False(result);
+                Assert.Equal(expectedErrorMessage, errorMessage);
+            }
+        }
+    }
+}
