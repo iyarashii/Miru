b7e111880447ac2f2340401b1abb6a29c660e2ad
Iyarashii <strider1337v@gmail.com>
2022-03-04 15:13:15 +0100

    Add test for LocalImageSource when image path is incorrect

--- Miru.Tests/ModelsTests/MiruAnimeModelTests.cs
+++ Miru.Tests/ModelsTests/MiruAnimeModelTests.cs
@@ -8,12 +8,15 @@ using Moq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 using Xunit;
 
 namespace Miru.Tests.ModelsTests
@@ -40,6 +43,40 @@ namespace Miru.Tests.ModelsTests
 
                 // Assert
                 Assert.Equal(expectedResult, sut.LocalImagePath);
+            }
+        }
+
+        //public static string TestFilePath = Path.Combine(Constants.ImageCacheFolderPath, "test.jpg");
+
+        //public static IEnumerable<object[]> GetLocalImageSourceTestData()
+        //{
+        //    var source = new BitmapImage();
+        //    source.BeginInit();
+        //    source.UriSource = new Uri(TestFilePath, UriKind.RelativeOrAbsolute);
+        //    source.CacheOption = BitmapCacheOption.OnLoad;
+        //    source.EndInit();
+        //    yield return new object[] { TestFilePath, source};
+        //}
+
+        //if (!File.Exists(TestFilePath))
+        //{
+        //    var testImage = new Bitmap(1, 1);
+        //    testImage.Save(TestFilePath, ImageFormat.Jpeg);
+        //}
+
+        [Fact]
+        public void LocalImageSource_GivenIncorrectLocalImagePath_ReturnsNull()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // Arrange
+                var sut = mock.Create<MiruAnimeModel>();
+
+                // Act
+                sut.LocalImagePath = "\\";
+
+                // Assert
+                Assert.Null(sut.LocalImageSource);
             }
         }
     }
