29e686e0d1742794e4d2c5469e08e92f82e8bc2b
Iyarashii <strider1337v@gmail.com>
2022-03-08 16:21:22 +0100

    Add test class for MiruAnimeModelExtensions

---
+++ Miru.Tests/ModelsTests/MiruAnimeModelExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using MiruLibrary.Models;
+using MiruLibrary;
+using Autofac.Extras.Moq;
+
+namespace Miru.Tests.ModelsTests
+{
+    public class MiruAnimeModelExtensionsTests
+    {
+        [Fact]
+        public void FilterByTitle_GivenEmptyString_DoesNothing()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // Arrange
+                var animeList = new List<MiruAnimeModel> { new MiruAnimeModel() };
+                var animeListExpected = animeList.ToList(); // create new list instance instead of copying reference
+
+                // Act
+                MiruAnimeModelExtensions.FilterByTitle(animeList, string.Empty);
+
+                // Assert
+                Assert.NotSame(animeListExpected, animeList);
+                Assert.Equal(animeListExpected, animeList);
+            }
+        }
+    }
+}
