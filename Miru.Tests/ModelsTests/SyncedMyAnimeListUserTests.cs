7cadbcc8a1f0e0b246bda814f47b36fa628f9e7b
Iyarashii <strider1337v@gmail.com>
2022-03-23 17:01:03 +0100

    Add test case for username shorter than min length

--- Miru.Tests/ModelsTests/SyncedMyAnimeListUserTests.cs
+++ Miru.Tests/ModelsTests/SyncedMyAnimeListUserTests.cs
@@ -29,5 +29,26 @@ namespace Miru.Tests.ModelsTests
                 Assert.Equal("The field Username must be a string or array type with a maximum length of '16'.", validationResults.First().ErrorMessage);
             }
         }
+
+        [Fact]
+        public void Username_ShorterThanMinLength_ValidationFails()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // Arrange
+                var sut = mock.Create<SyncedMyAnimeListUser>();
+                var valueToValidate = new string('x', 1);
+                var usernameContext = new ValidationContext(sut) { MemberName = nameof(sut.Username) };
+                var validationResults = new List<ValidationResult>();
+
+                // Act
+                var result = Validator.TryValidateProperty(valueToValidate, usernameContext, validationResults);
+
+                // Assert
+                Assert.False(result);
+                Assert.NotNull(validationResults);
+                Assert.Equal("The field Username must be a string or array type with a minimum length of '2'.", validationResults.First().ErrorMessage);
+            }
+        }
     }
 }
