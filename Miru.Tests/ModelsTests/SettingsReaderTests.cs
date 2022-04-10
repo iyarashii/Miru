9da31f9bf76434d8b7a9fe4cefe5a30884df8e02
Iyarashii <strider1337v@gmail.com>
2022-04-10 16:05:04 +0200

    Add test class

---
+++ Miru.Tests/ModelsTests/SettingsReaderTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Extras.Moq;
+using Xunit;
+using MiruLibrary;
+
+namespace Miru.Tests.ModelsTests
+{
+    public class SettingsReaderTests
+    {
+        // TODO: Add tests
+    }
+}
