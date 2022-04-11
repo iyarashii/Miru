4a218f17532d9a8a7fe13d1beedab6b76fc58bb5
Iyarashii <strider1337v@gmail.com>
2022-04-11 16:42:53 +0200

    Remove unused code from SettingsReader

--- MiruLibrary/Settings/SettingsReader.cs
+++ MiruLibrary/Settings/SettingsReader.cs
@@ -19,7 +19,6 @@ namespace MiruLibrary.Settings
         private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
         {
             ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
-            //ContractResolver = new SettingsReaderContractResolver(),
             ReferenceLoopHandling = ReferenceLoopHandling.Ignore
         };
 
@@ -33,8 +32,6 @@ namespace MiruLibrary.Settings
 
         public T Load<T>() where T : class, new() => Load(typeof(T)) as T;
 
-        //public T LoadSection<T>() where T : class, new() => LoadSection(typeof(T)) as T;
-
         public object Load(Type type)
         {
             if (!_fileSystemService.FileSystem.File.Exists(_configurationFilePath))
@@ -44,45 +41,5 @@ namespace MiruLibrary.Settings
 
             return JsonConvert.DeserializeObject(jsonFile, type, JsonSerializerSettings);
         }
-
-        //public object LoadSection(Type type)
-        //{
-        //    if (!File.Exists(_configurationFilePath))
-        //        return Activator.CreateInstance(type);
-
-        //    var jsonFile = File.ReadAllText(_configurationFilePath);
-        //    var section = ToCamelCase(type.Name.Replace(_sectionNameSuffix, string.Empty));
-        //    var settingsData = JsonConvert.DeserializeObject<dynamic>(jsonFile, JsonSerializerSettings);
-        //    var settingsSection = settingsData[section];
-
-        //    return settingsSection == null
-        //        ? Activator.CreateInstance(type)
-        //        : JsonConvert.DeserializeObject(JsonConvert.SerializeObject(settingsSection), type,
-        //            JsonSerializerSettings);
-        //}
-
-        //private class SettingsReaderContractResolver : DefaultContractResolver
-        //{
-        //    protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
-        //    {
-        //        var props = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-        //            .Select(p => CreateProperty(p, memberSerialization))
-        //            .Union(type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-        //                .Select(f => CreateProperty(f, memberSerialization)))
-        //            .ToList();
-        //        props.ForEach(p =>
-        //        {
-        //            p.Writable = true;
-        //            p.Readable = true;
-        //        });
-
-        //        return props;
-        //    }
-        //}
-
-        private static string ToCamelCase(string text)
-            => string.IsNullOrWhiteSpace(text)
-                ? string.Empty
-                : $"{text[0].ToString().ToLowerInvariant()}{text.Substring(1)}";
     }
 }
