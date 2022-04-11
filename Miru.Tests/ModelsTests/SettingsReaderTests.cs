7a9eacb9e62cbf244d0fe102709fbc711986bcb7
Iyarashii <strider1337v@gmail.com>
2022-04-11 17:27:23 +0200

    Add Load test for no config file case

--- Miru.Tests/ModelsTests/SettingsReaderTests.cs
+++ Miru.Tests/ModelsTests/SettingsReaderTests.cs
@@ -6,11 +6,38 @@ using System.Threading.Tasks;
 using Autofac.Extras.Moq;
 using Xunit;
 using MiruLibrary;
+using MiruLibrary.Settings;
+using MiruLibrary.Models;
+using Autofac;
 
 namespace Miru.Tests.ModelsTests
 {
     public class SettingsReaderTests
     {
-        // TODO: Add tests
+        [Fact]
+        public void Load_GivenNoConfigFilePresent_ReturnsDefaultUserSettings()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                // Arrange
+                mock.Mock<IFileSystemService>()
+                    .Setup(x => x.FileSystem.File.Exists(Constants.SettingsPath))
+                    .Returns(false);
+                var sut = mock.Create<SettingsReader>(
+                    new NamedParameter("fileSystemService", mock.Create<IFileSystemService>()), 
+                    new NamedParameter("configurationFilePath", Constants.SettingsPath), 
+                    new NamedParameter("sectionNameSuffix", "Settings"));
+
+                var expectedData = new UserSettings();
+
+                // Act
+                var result = (UserSettings)sut.Load(typeof(UserSettings));
+
+                // Assert
+                Assert.Equal(expectedData.AnimeImageSize, result.AnimeImageSize);
+                Assert.Equal(expectedData.DisplayedAnimeListType, result.DisplayedAnimeListType);
+                Assert.Equal(expectedData.DisplayedAnimeType, result.DisplayedAnimeType);
+            }
+        }
     }
 }
