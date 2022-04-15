d22bb38882d08ae26e7c7ac07937de7ff2b9a4c8
Iyarashii <strider1337v@gmail.com>
2022-04-15 20:43:10 +0200

    Add web service test class

---
+++ Miru.Tests/MyInternetConnectionLibraryTests/WebServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Autofac.Extras.Moq;
+using Moq;
+using MyInternetConnectionLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Miru.Tests.MyInternetConnectionLibraryTests
+{
+    public class WebServiceTests
+    {
+    }
+}
