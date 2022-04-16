1d009395905dee195594d82479ea5af53cd4dd46
Iyarashii <strider1337v@gmail.com>
2022-04-16 22:06:08 +0200

    Remove not needed usings

--- MyInternetConnectionLibrary/WebService.cs
+++ MyInternetConnectionLibrary/WebService.cs
@@ -1,8 +1,6 @@
 ﻿using JikanDotNet;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MyInternetConnectionLibrary
