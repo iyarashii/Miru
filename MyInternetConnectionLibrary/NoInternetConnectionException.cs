4a96882434ab9f748c447ea45fb14867e39dba80
Iyarashii <strider1337v@gmail.com>
2022-04-19 16:49:50 +0200

    Remove not used code and rename file

---
+++ MyInternetConnectionLibrary/NoInternetConnectionException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyInternetConnectionLibrary
+{
+    // custom exception thrown when internet connection issues occur
+    [Serializable]
+    public class NoInternetConnectionException : Exception
+    {
+        public NoInternetConnectionException()
+        {
+        }
+
+        public NoInternetConnectionException(string message) : base(message)
+        {
+        }
+
+        public NoInternetConnectionException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected NoInternetConnectionException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
