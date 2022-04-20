e0aa8d0df6f2422fc91a03ba4cf3a725f91ace32
Iyarashii <strider1337v@gmail.com>
2022-04-20 17:00:39 +0200

    Cleanup usings

--- Miru.Tests/MyInternetConnectionLibraryTests/WebServiceTests.cs
+++ Miru.Tests/MyInternetConnectionLibraryTests/WebServiceTests.cs
@@ -3,11 +3,6 @@ using JikanDotNet;
 using Moq;
 using MyInternetConnectionLibrary;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Miru.Tests.MyInternetConnectionLibraryTests
