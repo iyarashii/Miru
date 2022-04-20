8d9f002567ceabc93b3bc1035c914f2015a6bc27
Iyarashii <strider1337v@gmail.com>
2022-04-20 16:58:31 +0200

    Add TryToGetAnimeInfo test

--- Miru.Tests/MyInternetConnectionLibraryTests/WebServiceTests.cs
+++ Miru.Tests/MyInternetConnectionLibraryTests/WebServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac.Extras.Moq;
+using JikanDotNet;
 using Moq;
 using MyInternetConnectionLibrary;
 using System;
@@ -13,5 +14,21 @@ namespace Miru.Tests.MyInternetConnectionLibraryTests
 {
     public class WebServiceTests
     {
+        [Fact]
+        public async void TryToGetAnimeInfo_GivenValidMalId_ReturnsAnime()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                Func<IWebClientWrapper> mockWebClientFunc = () => { return Mock.Of<IWebClientWrapper>(); };
+                mock.Mock<IWebService>().Setup(x => x.CreateWebClient).Returns(mockWebClientFunc);
+                var testData = new Anime();
+                mock.Mock<IJikan>().Setup(x => x.GetAnime(It.IsAny<long>())).ReturnsAsync(testData);
+                var sut = mock.Create<WebService>();
+
+                var result = await sut.TryToGetAnimeInfo(default, default, mock.Mock<IJikan>().Object);
+
+                Assert.Equal(testData, result);
+            }
+        }
     }
 }
