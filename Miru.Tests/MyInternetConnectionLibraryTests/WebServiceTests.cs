6d6324f4fdb71c95f2f414f873b8746ece3ceb88
Iyarashii <strider1337v@gmail.com>
2022-04-22 16:49:59 +0200

    Add test case for HttpRequestException

--- Miru.Tests/MyInternetConnectionLibraryTests/WebServiceTests.cs
+++ Miru.Tests/MyInternetConnectionLibraryTests/WebServiceTests.cs
@@ -4,6 +4,7 @@ using JikanDotNet.Exceptions;
 using Moq;
 using MyInternetConnectionLibrary;
 using System;
+using System.Net.Http;
 using Xunit;
 
 namespace Miru.Tests.MyInternetConnectionLibraryTests
@@ -47,6 +48,27 @@ namespace Miru.Tests.MyInternetConnectionLibraryTests
 
                 mock.Mock<IJikan>().Verify(x => x.GetAnime(It.IsAny<long>()), Times.Exactly(2));
                 Assert.Equal(testData, result);
+            }
+        }
+
+        [Fact]
+        public async void TryToGetAnimeInfo_GivenHttpRequestException_ThrowsNoInternetConnectionException()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                Func<IWebClientWrapper> mockWebClientFunc = () => { return Mock.Of<IWebClientWrapper>(); };
+                mock.Mock<IWebService>()
+                    .Setup(x => x.CreateWebClient)
+                    .Returns(mockWebClientFunc);
+                mock.Mock<IJikan>()
+                    .Setup(x => x.GetAnime(It.IsAny<long>()))
+                    .ThrowsAsync(new HttpRequestException());
+                var sut = mock.Create<WebService>();
+
+                var result = await Assert.ThrowsAsync<NoInternetConnectionException>(
+                    () => sut.TryToGetAnimeInfo(default, default, mock.Mock<IJikan>().Object)
+                    );
+                Assert.Equal("No internet connection", result.Message);
             }
         }
     }
