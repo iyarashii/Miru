1e10bda32ce12f8117a804f3786fa6f8ce29b12e
Iyarashii <strider1337v@gmail.com>
2022-04-30 19:18:10 +0200

    Fix usings

--- Miru/Views/TextBoxHelper.cs
+++ Miru/Views/TextBoxHelper.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
 namespace Miru.Views
 {
+    // TODO: add unit tests
     public class TextBoxHelper : DependencyObject
     {
         public class MvvmCommand : DependencyObject, ICommand
