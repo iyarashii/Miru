8f3d1171aa020823653ebcd46c9a329febaaec8f
Iyarashii <strider1337v@gmail.com>
2022-05-15 15:17:22 +0200

    Add constructor tests

---
+++ Miru.Tests/ViewsTests/MvvmCommandTests.cs
@@ -0,0 +1,27 @@
+﻿using Miru.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Miru.Tests.ViewsTests
+{
+    public class MvvmCommandTests
+    {
+        [Fact]
+        public void Constructor_GivenNullExecute_ThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>("execute", () => new MvvmCommand(null));
+        }
+
+        [Fact]
+        public void Constructor_GivenNullCanExecute_SetCanExecuteTrue()
+        {
+            var sut = new MvvmCommand(x => { }, null);
+            
+            Assert.True(sut._canExecute(null));
+        }
+    }
+}
