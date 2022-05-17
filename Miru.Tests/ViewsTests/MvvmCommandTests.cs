1172f971a71dfed6ca397e3177e446c1e914ad9e
Iyarashii <strider1337v@gmail.com>
2022-05-17 19:54:17 +0200

    Remove not needed usings

--- Miru.Tests/ViewsTests/MvvmCommandTests.cs
+++ Miru.Tests/ViewsTests/MvvmCommandTests.cs
@@ -1,9 +1,5 @@
 ﻿using Miru.Views;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Miru.Tests.ViewsTests
