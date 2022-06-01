4149cc921a0faa87b0738acdf4b1a8d2474de3c4
Iyarashii <strider1337v@gmail.com>
2022-06-01 18:46:27 +0200

    Add `GetVisible` tests

---
+++ Miru.Tests/ViewsTests/RunVisibilityTests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) 2022 iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0,
+// go to https://github.com/iyarashii/Miru/blob/master/LICENSE for full license details.
+
+using Miru.Views;
+using System;
+using System.Windows.Documents;
+using Xunit;
+
+namespace Miru.Tests.ViewsTests
+{
+    public class RunVisibilityTests
+    {
+        [Fact]
+        public void GetVisible_DefaultRun_ReturnVisiblePropertyValue()
+        {
+            Run testData = new Run();
+
+            var result = RunVisibility.GetVisible(testData);
+        
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void GetVisible_NullRun_ThrowException()
+        {
+            Assert.Throws<NullReferenceException>(() => RunVisibility.GetVisible(null));
+        }
+    }
+}
