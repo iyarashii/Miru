69aa24dfd042a2b3d4ca015259040e9e96528454
Iyarashii <strider1337v@gmail.com>
2022-06-06 17:12:23 +0200

    Remove redundant null check and update tests

--- Miru/Views/MvvmCommand.cs
+++ Miru/Views/MvvmCommand.cs
@@ -40,7 +40,7 @@ namespace Miru.Views
         }
         public virtual bool CanExecute(object parameter)
         {
-            return _canExecute == null || _canExecute(parameter);
+            return _canExecute(parameter);
         }
     }
 }
