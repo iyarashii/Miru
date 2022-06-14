73fee7348ad31dd8820ddd0c286333521d6a50fb
Iyarashii <strider1337v@gmail.com>
2022-06-14 19:43:16 +0200

    Exclude from code coverage

--- Miru/ViewModels/Interfaces/ISimpleContentDialog.cs
+++ Miru/ViewModels/Interfaces/ISimpleContentDialog.cs
@@ -3,6 +3,7 @@
 // go to https://github.com/iyarashii/Miru/blob/master/LICENSE for full license details.
 
 using ModernWpf.Controls;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
 namespace Miru.ViewModels
@@ -14,6 +15,8 @@ namespace Miru.ViewModels
         string PrimaryButtonText { get; set; }
         string CloseButtonText { get; set; }
         ContentDialogButton DefaultButton { get; set; }
+
+        [ExcludeFromCodeCoverage]
         void Config(object title, string primaryButtonText = "Yes", string closeButtonText = "No",
             ContentDialogButton defaultButton = ContentDialogButton.Primary, object content = null, string secondaryButtonText = "");
     }
