9e4e94f5fcda551de4c35e188feb1b0b0f958817
Iyarashii <strider1337v@gmail.com>
2022-06-16 12:46:23 +0200

    Cleanup usings

--- MiruLibrary/Models/UserSettings.cs
+++ MiruLibrary/Models/UserSettings.cs
@@ -2,12 +2,6 @@
 // Licensed under the GNU General Public License v3.0,
 // go to https://github.com/iyarashii/Miru/blob/master/LICENSE for full license details.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace MiruLibrary.Models
 {
     public class UserSettings
