ff9e97e26104c5d8ccfa5c53ffe16a368052464f
Iyarashii <strider1337v@gmail.com>
2022-07-02 12:00:34 +0200

    Add tests for `Convert`

--- Miru.Tests/ViewsTests/DroppedToColorConverterTests.cs
+++ Miru.Tests/ViewsTests/DroppedToColorConverterTests.cs
@@ -3,7 +3,10 @@
 // go to https://github.com/iyarashii/Miru/blob/master/LICENSE for full license details.
 
 using Miru.Views;
+using MiruLibrary;
 using System;
+using System.Windows;
+using System.Windows.Media;
 using Xunit;
 
 namespace Miru.Tests.ViewsTests
@@ -16,6 +19,47 @@ namespace Miru.Tests.ViewsTests
             var sut = new DroppedToColorConverter();
 
             Assert.Throws<NotSupportedException>(() => sut.ConvertBack(default, default, default, default));
+        }
+
+        [Theory]
+        [InlineData(AnimeListType.Senpai)]
+        [InlineData(AnimeListType.Season)]
+        public void Convert_DroppedTrueAndSeasonListType_ReturnRedColor(AnimeListType animeListType)
+        {
+            var testValues = new object[] { true, animeListType};
+            var sut = new DroppedToColorConverter();
+
+            var result = sut.Convert(testValues, typeof(Color), default, default);
+
+            Assert.Equal(Brushes.Red.Color, result);
+        }
+
+        [Theory]
+        [InlineData(AnimeListType.Senpai)]
+        [InlineData(AnimeListType.Season)]
+        public void Convert_WatchingTrueAndSeasonListType_ReturnGreenColor(AnimeListType animeListType)
+        {
+            var testValues = new object[] { false, animeListType, true };
+            var sut = new DroppedToColorConverter();
+
+            var result = sut.Convert(testValues, typeof(Color), default, default);
+
+            Assert.Equal(Brushes.Green.Color, result);
+        }
+
+        [Theory]
+        [InlineData(AnimeListType.Senpai)]
+        [InlineData(AnimeListType.Season)]
+        [InlineData(AnimeListType.AiringAndWatching)]
+        [InlineData(AnimeListType.Watching)]
+        public void Convert_NotSeasonListTypeOrBothDroppedAndWatchingFalse_ReturnUnsetValue(AnimeListType animeListType)
+        {
+            var testValues = new object[] { false, animeListType, false };
+            var sut = new DroppedToColorConverter();
+
+            var result = sut.Convert(testValues, typeof(Color), default, default);
+
+            Assert.Equal(DependencyProperty.UnsetValue, result);
         }
     }
 }
