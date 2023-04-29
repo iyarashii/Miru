267953a5e296b15f77b6a017c1c08367896a9b14
Iyarashii <strider1337v@gmail.com>
2023-04-29 22:53:56 +0200

    Cleanup

--- Miru.Tests/UI/CopyOpEdTests.cs
+++ Miru.Tests/UI/CopyOpEdTests.cs
@@ -7,10 +7,7 @@ using FlaUI.Core.Conditions;
 using FlaUI.Core.Input;
 using FlaUI.UIA2;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Miru.Tests.UI
