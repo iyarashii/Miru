bbae0e3897ebdd2414a390d925de0128d7078b59
Iyarashii <strider1337v@gmail.com>
2023-05-05 18:57:09 +0200

    Remove date

--- Miru.Tests/UI/CopyOpEdTests.cs
+++ Miru.Tests/UI/CopyOpEdTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2023 iyarashii @ https://github.com/iyarashii 
+﻿// Copyright iyarashii @ https://github.com/iyarashii 
 // Licensed under the GNU General Public License v3.0,
 // go to https://github.com/iyarashii/Miru/blob/master/LICENSE for full license details.
 
