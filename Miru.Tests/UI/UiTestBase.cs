bf67bc85e4549389c7a6f1a24e5fad39956ea2a4
Iyarashii <strider1337v@gmail.com>
2023-05-08 22:48:20 +0200

    Cleanup

--- Miru.Tests/UI/UiTestBase.cs
+++ Miru.Tests/UI/UiTestBase.cs
@@ -6,10 +6,6 @@ using FlaUI.Core.AutomationElements;
 using FlaUI.Core.Input;
 using FlaUI.UIA2;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Miru.Tests.UI
 {
