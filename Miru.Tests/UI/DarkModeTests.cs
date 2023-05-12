44d64d7ea98d116811d5c40d86a181420f827ee7
Iyarashii <strider1337v@gmail.com>
2023-05-12 16:56:17 +0200

    Add dark mode toggle ui tests class

---
+++ Miru.Tests/UI/DarkModeTests.cs
@@ -0,0 +1,34 @@
+﻿// Copyright iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0,
+// go to https://github.com/iyarashii/Miru/blob/master/LICENSE for full license details.
+
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Miru.Tests.UI
+{
+    public class DarkModeTests : UiTestBase
+    {
+        [Fact]
+        public void TurnOffDarkMode()
+        {
+            // Arrange
+            var darkModeSwitchThumb = mainWindow.FindAllByXPath("/Button[5]/Thumb").FirstOrDefault();
+            Assert.NotNull(darkModeSwitchThumb);
+
+            // Act
+            darkModeSwitchThumb.Click();
+
+            // Assert 
+            Wait.UntilInputIsProcessed(new TimeSpan(0, 0, 2));
+            var darkModeButton = mainWindow.FindAllByXPath("/Button[5]").FirstOrDefault();
+            Assert.False(darkModeButton.AsToggleButton().IsToggled);
+        }
+    }
+}
