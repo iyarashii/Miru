38214190fe31cc07c09867864080ae5c9a19d115
Iyarashii <strider1337v@gmail.com>
2023-08-14 18:23:42 +0200

    Try to start appium server before each test

--- Miru.Tests/UI/AppiumTests/AppiumUiTestBase.cs
+++ Miru.Tests/UI/AppiumTests/AppiumUiTestBase.cs
@@ -6,6 +6,10 @@ using System;
 using Xunit;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Appium.Service;
+using OpenQA.Selenium.Appium.Service.Options;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Miru.Tests.UI.AppiumTests
 {
@@ -13,8 +17,39 @@ namespace Miru.Tests.UI.AppiumTests
     public class AppiumUiTestBase : IDisposable
     {
         protected WindowsDriver<WindowsElement> appSession;
+        //private static AppiumLocalService service;
+        private Process server;
         public AppiumUiTestBase()
         {
+            //var argCollector = new OptionCollector()
+            //            .AddArguments(new System.Collections.Generic.KeyValuePair<string, string>("", ""));
+            //var builder = new AppiumServiceBuilder().WithAppiumJS().WithArguments(argCollector);
+            //var builder = new AppiumServiceBuilder().WithAppiumJS(new System.IO.FileInfo("C:\\Users\\Iyarashii\\AppData\\Roaming\\nvm\\v20.5.1\\appium.cmd"));
+            //service = builder.Build();
+            //service.Start();
+            //server = Process.Start("C:\\Users\\Iyarashii\\AppData\\Roaming\\nvm\\v20.5.1\\appium.cmd");
+            var cmdsi = new ProcessStartInfo("pwsh.exe");
+            cmdsi.Arguments = "-noexit -command \"appium\"";
+            //cmdsi.Arguments = "appium";
+            //cmdsi.RedirectStandardOutput = true;
+            //cmdsi.RedirectStandardError = true;
+            //cmdsi.UseShellExecute = false;
+            //cmdsi.RedirectStandardOutput = true;
+            //cmdsi.WindowStyle = ProcessWindowStyle.Maximized;
+            server = Process.Start(cmdsi);
+            Task.Delay(5000).Wait();
+            var output = string.Empty;
+            //while(output.Contains("[Appium] No plugins have been installed. Use the \"appium plugin\" command to install the one(s) you want to use.") == false)
+            //{
+            //    try
+            //    {
+            //        Task.Delay(1000).Wait();
+            //        output = server.StandardOutput.ReadToEnd();
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //    }
+            //}
             AppiumOptions appCapabilities = new AppiumOptions();
             appCapabilities.AddAdditionalCapability("appium:app", "G:\\repos\\Miru\\Miru\\bin\\Debug\\app.publish\\Miru.exe");
             appCapabilities.AddAdditionalCapability("platformName", "Windows");
@@ -24,6 +59,7 @@ namespace Miru.Tests.UI.AppiumTests
         public void Dispose()
         {
             appSession.Close();
+            server.CloseMainWindow();
         }
     }
 }
