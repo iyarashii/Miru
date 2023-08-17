f3d5e3ef805a7fc5419ba0b826d50b37f52cf28e
Iyarashii <strider1337v@gmail.com>
2023-08-17 19:53:43 +0200

    Add 2nd test project

---
+++ Miru.UiTests/Usings.cs
@@ -0,0 +1,5 @@
+// Copyright iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0,
+// go to https://github.com/iyarashii/Miru/blob/master/LICENSE for full license details.
+
+global using Xunit;
