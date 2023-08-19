f1727f1b93bd4fe010624aab8b305b881a047ad5
Iyarashii <strider1337v@gmail.com>
2023-08-19 16:36:23 +0200

    Add clear cache ui tests in appium

---
+++ Miru.Tests/UI/AppiumTests/ClearCacheTests.cs
@@ -0,0 +1,24 @@
+﻿// Copyright iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0,
+// go to https://github.com/iyarashii/Miru/blob/master/LICENSE for full license details.
+
+using Xunit;
+
+namespace Miru.Tests.UI.AppiumTests
+{
+    public class ClearCacheTests : AppiumUiTestBase
+    {
+        [Fact]
+        public void CheckClearCacheDialogButtons()
+        {
+            var button = appSession.FindElementByName("Clear Cache");
+            Assert.NotNull(button);
+            button.Click();
+            var closeButton = appSession.FindElementByName("No");
+            var primaryButton = appSession.FindElementByName("Yes");
+            Assert.NotNull(closeButton);
+            Assert.NotNull(primaryButton);
+            closeButton.Click();
+        }
+    }
+}
