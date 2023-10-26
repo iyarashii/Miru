63215d61c12255252a19add9a5d8aa1157715ec6
Iyarashii <strider1337v@gmail.com>
2023-10-26 19:05:22 +0200

    Add custom content to allow text selection

---
+++ Miru/Views/OpEdCustomContent.xaml.cs
@@ -0,0 +1,32 @@
+﻿// Copyright iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0,
+// go to https://github.com/iyarashii/Miru/blob/master/LICENSE for full license details.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Miru.Views
+{
+    /// <summary>
+    /// Interaction logic for OpEdCustomContent.xaml
+    /// </summary>
+    public partial class OpEdCustomContent : UserControl
+    {
+        public OpEdCustomContent()
+        {
+            InitializeComponent();
+        }
+    }
+}
