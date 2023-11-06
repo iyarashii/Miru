96aed1b997e97d8751d6a002a0c2521577d5f34c
Iyarashii <strider1337v@gmail.com>
2023-11-06 21:03:05 +0100

    Remove commented out code

--- Miru/Views/OpEdCustomContent.xaml.cs
+++ Miru/Views/OpEdCustomContent.xaml.cs
@@ -31,7 +31,6 @@ namespace Miru.Views
             InitializeComponent();
             _shellViewModel = shellViewModelRef;    
             AddButtons(opThemes, edThemes);
-            //OpEdTextBox.MouseDoubleClick += OpEdTextBox_MouseDoubleClick;
         }
         private void AddButtons(string opThemes, string edThemes)
         {
@@ -40,53 +39,25 @@ namespace Miru.Views
             Run opHeader = new Run(opThemes.Split('\n')[0]);
             Run edHeader = new Run(edThemes.Split('\n')[0]);
             paragraph.Inlines.Add(opHeader);
-            //OpEdTextBlock.Inlines.Add(opHeader);
             paragraph.Inlines.Add(new LineBreak());
-            //OpEdTextBlock.Inlines.Add(new LineBreak());
             // - 2 is to ignore \n after OP/ED and at the end so dont use [0] and [last] indexes
             int numberOfOpLines = opThemes.Split('\n').Length - 2;
             int numberOfEdLines = edThemes.Split('\n').Length - 2;
             for (int i = 1; i <= numberOfOpLines; i++)
             {
-                //Button btn = new Button
-                //{
-                //    Content = "Copy",
-                //    Tag = opThemes.Split('\n')[i]
-                //};
-                //btn.Click += CopyOpEdSongNameAndArtist;
-                //InlineUIContainer btnContainer = new InlineUIContainer(btn);
                 Run opThemeName = new Run(opThemes.Split('\n')[i]);
                 opThemeName.MouseDown += CopyOpEdSongNameAndArtist;
-                //OpEdTextBlock.Inlines.Add(opThemeName);
-                //OpEdTextBlock.Inlines.Add(btnContainer);
-                //OpEdTextBlock.Inlines.Add(new LineBreak());
                 paragraph.Inlines.Add(opThemeName);
-                //paragraph.Inlines.Add(btnContainer);
                 paragraph.Inlines.Add(new LineBreak());
-                //ButtonsPanel.Children.Add(btn);
             }
-            //OpEdTextBlock.Inlines.Add(edHeader);
-            //OpEdTextBlock.Inlines.Add(new LineBreak());
             paragraph.Inlines.Add(edHeader);
             paragraph.Inlines.Add(new LineBreak());
             for (int i = 1; i <= numberOfEdLines; i++)
             {
-                //Button btn = new Button
-                //{
-                //    Content = "Copy",
-                //    Tag = edThemes.Split('\n')[i]
-                //};
-                //btn.Click += CopyOpEdSongNameAndArtist;
-                //InlineUIContainer btnContainer = new InlineUIContainer(btn);
                 Run edThemeName = new Run(edThemes.Split('\n')[i]);
                 edThemeName.MouseDown += CopyOpEdSongNameAndArtist;
-                //OpEdTextBlock.Inlines.Add(edThemeName);
-                //OpEdTextBlock.Inlines.Add(btnContainer);
-                //OpEdTextBlock.Inlines.Add(new LineBreak());
                 paragraph.Inlines.Add(edThemeName);
-                //paragraph.Inlines.Add(btnContainer);
                 paragraph.Inlines.Add(new LineBreak());
-                //ButtonsPanel.Children.Add(btn);
             }
             flowDoc.Blocks.Add(paragraph);
             OpEdTextBox.Document = flowDoc;
@@ -100,35 +71,5 @@ namespace Miru.Views
                 || e.ChangedButton == MouseButton.Left && e.ClickCount == 2))
                 _shellViewModel.CopyAnimeTitleToClipboard(_shellViewModel.GetSongTitleAndArtistName((sender as Run).Text));
         }
-
-        //private void OpEdTextBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        //{
-        //    var runClicked = FindRunFromMousePosition(e.GetPosition((RichTextBox)sender), (RichTextBox)sender);
-        //    if (runClicked != null)
-        //    {
-        //        // Handle the double-click on the specific Run element (runClicked)
-        //        // Your logic here
-        //        _shellViewModel.CopyAnimeTitleToClipboard(_shellViewModel.GetSongTitleAndArtistName(runClicked.Text));
-        //    }
-        //}
-
-        //private Run FindRunFromMousePosition(Point clickPoint, RichTextBox textBox)
-        //{
-        //    int cp = 0;
-        //    var inlines = (textBox.Document.Blocks.FirstBlock as Paragraph).Inlines;
-        //    for (int i = 0; i < inlines.Count; i++)
-        //    {
-        //        if (inlines.ElementAt(i) is Run run)
-        //        {
-        //            cp += run.Text.Length;
-        //            var rect = run.ContentStart.GetCharacterRect(LogicalDirection.Forward);
-        //            if (clickPoint.X >= rect.Left && clickPoint.X <= rect.Right)
-        //            {
-        //                return run;
-        //            }
-        //        }
-        //    }
-        //    return null;
-        //}
     }
 }
