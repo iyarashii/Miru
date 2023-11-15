b474a0852004a05bebdd1d769504ea6d632fa73c
Iyarashii <strider1337v@gmail.com>
2023-11-15 22:53:19 +0100

    Cleanup

--- Miru.UiTests/UI/AppiumTests/AppiumUiTestBase.cs
+++ Miru.UiTests/UI/AppiumTests/AppiumUiTestBase.cs
@@ -2,20 +2,10 @@
 // Licensed under the GNU General Public License v3.0,
 // go to https://github.com/iyarashii/Miru/blob/master/LICENSE for full license details.
 
-using System;
-using Xunit;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 using System.Diagnostics;
-using FlaUI.UIA2;
-using FlaUI.Core.Tools;
-using FlaUI.Core.AutomationElements;
-using System.Collections.Generic;
-using FlaUI.UIA3;
-using Namotion.Reflection;
-using OpenQA.Selenium;
 using System.Net;
-using System.Net.Http.Json;
 
 namespace Miru.Tests.UI.AppiumTests
 {
@@ -23,27 +13,13 @@ namespace Miru.Tests.UI.AppiumTests
     public class AppiumUiTestBase : IDisposable
     {
         protected WindowsDriver appSession;
-        private readonly Process appiumServerProcess;
-        //private readonly Window mainWindow;
         public AppiumUiTestBase()
         {
             var cmdsi = new ProcessStartInfo("wt.exe")
             {
                 Arguments = "-p \"appium\""
             };
-            // commented out code is for powershell without windowsterminal.exe installed
-            //var cmdsi = new ProcessStartInfo("pwsh.exe")
-            //{
-            //    Arguments = "-noexit -command \"appium\""
-            //};
-            appiumServerProcess = Process.Start(cmdsi);
-            //var flauiSP = FlaUI.Core.Application.Attach(appiumServerProcess);
-            //var flauiSP = Retry.WhileException(() => FlaUI.Core.Application.Attach("WindowsTerminal.exe"),
-            //   interval: TimeSpan.FromSeconds(1),
-            //   timeout: TimeSpan.FromMinutes(1)).Result;
-            //mainWindow = flauiSP.GetMainWindow(new UIA3Automation());
-            //var textArea = mainWindow.FindFirstDescendant(x => x.ByName("appium"));
-            //textArea.busy();
+            Process.Start(cmdsi);
             var httpClient = new HttpClient();
             bool serverIsResponding = false;
             int count = 0;
@@ -69,23 +45,16 @@ namespace Miru.Tests.UI.AppiumTests
 
             AppiumOptions appCapabilities = new AppiumOptions
             {
-                //var app = Process.Start(Environment.GetEnvironmentVariable("MIRU_PATH", EnvironmentVariableTarget.Machine));
-                //Wait.UntilInputIsProcessed(TimeSpan.FromSeconds(5));
                 App = Environment.GetEnvironmentVariable("MIRU_PATH", EnvironmentVariableTarget.Machine),
-                //appCapabilities.AddAdditionalAppiumOption("appium:appTopLevelWindow", app.MainWindowHandle.ToString("x"));
                 PlatformName = "Windows",
                 AutomationName = "Windows"
             };
-            // old way to add appium options below
-            //appCapabilities.AddAdditionalAppiumOption("platformName", "Windows");
-            //appCapabilities.AddAdditionalAppiumOption("appium:automationName", "Windows");
             appSession = new WindowsDriver(new Uri("http://127.0.0.1:4723/"), appCapabilities);
         }
         public void Dispose()
         {
             appSession.Close();
             Process.GetProcessesByName("WindowsTerminal").ToList().ForEach(x => x.Kill());
-            //mainWindow.Close();
         }
 
         public void RightClick(string elementId)
