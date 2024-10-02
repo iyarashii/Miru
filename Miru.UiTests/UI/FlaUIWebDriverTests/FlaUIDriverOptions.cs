d2e4ac33524d6b095b2c029891e4ffcda80b1f5b
Iyarashii <strider1337v@gmail.com>
2024-10-02 23:10:01 +0200

    Add initial prep for flaui webdriver testing

---
+++ Miru.UiTests/UI/FlaUIWebDriverTests/FlaUIDriverOptions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0,
+// go to https://github.com/iyarashii/Miru/blob/master/LICENSE for full license details.
+
+using OpenQA.Selenium.Appium;
+
+namespace Miru.UiTests.UI.FlaUIWebDriverTests
+{
+    public class FlaUIDriverOptions : AppiumOptions
+    {
+        public static FlaUIDriverOptions ForApp(string path)
+        {
+            return new FlaUIDriverOptions()
+            {
+                PlatformName = "windows",
+                AutomationName = "flaui",
+                App = path
+            };
+        }
+    }
+}
---
+++ Miru.UiTests/UI/FlaUIWebDriverTests/FlaUIWebDriverTestBase.cs
@@ -0,0 +1,17 @@
+﻿// Copyright iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0,
+// go to https://github.com/iyarashii/Miru/blob/master/LICENSE for full license details.
+
+using OpenQA.Selenium.Appium.Windows;
+
+namespace Miru.UiTests.UI.FlaUIWebDriverTests
+{
+    public class FlaUIWebDriverTestBase
+    {
+        public FlaUIWebDriverTestBase()
+        {
+            var pathToApp = Environment.GetEnvironmentVariable("MIRU_PATH", EnvironmentVariableTarget.Machine);
+            var driver = new WindowsDriver(new Uri("http://localhost:4723"), FlaUIDriverOptions.ForApp(pathToApp));
+        }
+    }
+}
