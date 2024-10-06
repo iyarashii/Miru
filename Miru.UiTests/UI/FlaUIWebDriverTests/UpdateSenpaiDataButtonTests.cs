516a08ddf9900cd654ed727745240d3a2d49b3d3
Iyarashii <strider1337v@gmail.com>
2024-10-06 21:33:06 +0200

    Add senpai data button test flau web driver

---
+++ Miru.UiTests/UI/FlaUIWebDriverTests/UpdateSenpaiDataButtonTests.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+
+namespace Miru.UiTests.UI.FlaUIWebDriverTests
+{
+    public class UpdateSenpaiDataButtonTests : FlaUIWebDriverTestBase
+    {
+        [Fact]
+        public void CheckDialogButtonsAfterPress()
+        {
+            var button = driver.FindElement(By.Name("Update Senpai Data"));
+            Assert.NotNull(button);
+            button.Click();
+            var closeButton = driver.FindElement(By.Name("No"));
+            var primaryButton = driver.FindElement(By.Name("Yes"));
+            Assert.NotNull(closeButton);
+            Assert.NotNull(primaryButton);
+            closeButton.Click();
+        }
+    }
+}
