a862731562a94690c1cfb2737a500946349055e0
Iyarashii <strider1337v@gmail.com>
2025-04-11 23:59:01 +0200

    Add age rating to model

--- MiruLibrary/Models/MiruAnimeModel.cs
+++ MiruLibrary/Models/MiruAnimeModel.cs
@@ -66,6 +66,7 @@ namespace MiruLibrary.Models
         // is the anime currently airing status flag
         public bool CurrentlyAiring { get; set; }
         public int? WatchedEpisodes { get; set; }
+        public AgeRating AgeRating { get; set; } // "G", "PG", "R" etc.
 
         // "Movie", "TV" etc.
         public string Type { get; set; }
